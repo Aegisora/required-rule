402401eff2ebf05668d52c097c8da7cf6196034d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-26 21:27:27 +0400

    [AEGISORA-69222470] Implemented executeValidate method.

--- src/RequiredRule.php
+++ src/RequiredRule.php
@@ -2,10 +2,21 @@
 
 namespace Aegisora\Rules;
 
-class RequiredRule
+use Aegisora\RuleContract\Models\Context;
+use Aegisora\RuleContract\Models\Result;
+use Aegisora\RuleContract\Rule;
+
+class RequiredRule extends Rule
 {
     public static function create(): self
     {
         return new self();
+    }
+
+    protected function executeValidate(Context $context): Result
+    {
+        return ($context->getValue() !== null) ?
+            $this->getDefaultValidResult() :
+            $this->getDefaultInvalidResult();
     }
 }
